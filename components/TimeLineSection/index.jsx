1c28169128e59ad131b0631dd755be3767572822
arcodez <abelileitor123@gmail.com>
2023-04-15 12:19:25 -0400

    update dates

--- components/TimeLineSection/index.jsx
+++ components/TimeLineSection/index.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import TimeLineElement from "../TimeLineElement";
 
 const TimeLineInfo = [
-  { date: "20/03/2010", image: "camila_piscina.jpg" },
-
-  { date: "20/03/2010", image: "cami_delfin.jpg" },
-  { date: "20/03/2010", image: "cami_peque_seat.jpg" },
-  { date: "20/03/2010", image: "cami_peque_headphones.jpg" },
-  { date: "20/03/2010", image: "cami_peque_church.jpg" },
-  { date: "20/03/2010", image: "cami_medium_with_dog.jpg" },
-  { date: "20/03/2010", image: "cami_medium_model.jpg" },
-  { date: "20/03/2010", image: "cami_medium_flower.jpg" },
-  { date: "20/03/2010", image: "cami_medium_green.jpg" },
-  { date: "20/03/2010", image: "cami_end_selfie.jpg" },
-  { date: "20/03/2010", image: "cami_end2.jpg" },
-  { date: "20/03/2010", image: "cami_end3.jpg" },
-  { date: "20/03/2010", image: "cami_end4.jpg" },
-  { date: "20/03/2010", image: "cami_end5.jpg" },
+  { date: "25/05/2014", image: "camila_piscina.jpg" },
+  { date: "07/05/2015", image: "cami_delfin.jpg" },
+  { date: "19/08/2014", image: "cami_peque_seat.jpg" },
+  { date: "24/02/2016", image: "cami_peque_headphones.jpg" },
+  { date: "25/05/2014", image: "cami_peque_church.jpg" },
+  { date: "11/03/2021", image: "cami_medium_with_dog.jpg" },
+  { date: "26/03/2020", image: "cami_medium_model.jpg" },
+  { date: "17/12/2016", image: "cami_medium_flower.jpg" },
+  { date: "01/09/2018", image: "cami_medium_green.jpg" },
+  { date: "19/12/2021", image: "cami_end_selfie.jpg" },
+  { date: "19/12/2021", image: "cami_end2.jpg" },
+  { date: "16/04/2022", image: "cami_end3.jpg" },
+  { date: "21/12/2021", image: "cami_end4.jpg" },
+  { date: "22/01/2023", image: "cami_end5.jpg" },
 ];
 
 const TimeLineSection = () => {
